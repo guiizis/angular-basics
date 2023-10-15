0bf1fc3ca1f0cde3ea7af1ab8b281e284121a795
guiziis <guigamoda@hotmail.com>
2023-10-15 17:20:35 -0300

    feat: adiciona estrutura recipe-list

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.interface';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    {
+      name: 'teste',
+      description: 'teste',
+      imagePath: 'https://www.foodandwine.com/thmb/-wrVEmLnndHnIzla-W2g2x-LLQA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Korean-Style-Seared-Tuna-FT-RECIPE0822-2000-1e6d136c69684c0a9e31f584b3161a81.jpg'
+    }
+  ];
 
 }
