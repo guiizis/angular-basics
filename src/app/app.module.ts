cc4e95a5198d9f4cafaf9dee5d3ea638d6c8ec90
guiziis <guigamoda@hotmail.com>
2023-10-30 09:05:12 -0300

    feat: adiciona funcionalidade a recipe-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { RecipeBookModule } from './recipe-book/recipe-book.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { HeaderModule } from './header/header.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     RecipeBookModule,
     ShoppingListModule,
