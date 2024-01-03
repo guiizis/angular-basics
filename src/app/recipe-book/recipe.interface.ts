e8fe02a4adbc44864b9eef137f318f52088985b9
guiziis <guigamoda@hotmail.com>
2024-01-03 08:05:34 -0300

    feat: adiciona ingridients ao recipe detail

--- src/app/recipe-book/recipe.interface.ts
+++ src/app/recipe-book/recipe.interface.ts
@@ -1,5 +1,8 @@
+import { Ingredients } from "../shared/ingredients.interface";
+
 export interface Recipe {
   name: string;
   description: string;
   imagePath: string;
+  ingridients: Ingredients[];
 }
--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -12,12 +12,28 @@ export class RecipeService {
     {
       name: 'teste',
       description: 'teste',
-      imagePath: 'https://www.foodandwine.com/thmb/-wrVEmLnndHnIzla-W2g2x-LLQA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Korean-Style-Seared-Tuna-FT-RECIPE0822-2000-1e6d136c69684c0a9e31f584b3161a81.jpg'
+      imagePath: 'https://www.foodandwine.com/thmb/-wrVEmLnndHnIzla-W2g2x-LLQA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Korean-Style-Seared-Tuna-FT-RECIPE0822-2000-1e6d136c69684c0a9e31f584b3161a81.jpg',
+      ingridients: [
+        {
+          name: 'Meat',
+          amount: 5
+        }
+      ]
     },
     {
       name: 'outro teste',
       description: 'mais um teste',
-      imagePath: 'https://www.foodandwine.com/thmb/-wrVEmLnndHnIzla-W2g2x-LLQA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Korean-Style-Seared-Tuna-FT-RECIPE0822-2000-1e6d136c69684c0a9e31f584b3161a81.jpg'
+      imagePath: 'https://www.foodandwine.com/thmb/-wrVEmLnndHnIzla-W2g2x-LLQA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Korean-Style-Seared-Tuna-FT-RECIPE0822-2000-1e6d136c69684c0a9e31f584b3161a81.jpg',
+      ingridients: [
+        {
+          name: 'French Fries',
+          amount: 20
+        },
+        {
+          name: 'Buns',
+          amount: 3
+        }
+      ]
     }
   ];
 
