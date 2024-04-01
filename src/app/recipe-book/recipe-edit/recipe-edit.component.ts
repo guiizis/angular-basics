32d8c5740434692f137a05aa02a08a9c2f8a0902
guiziis <guigamoda@hotmail.com>
2024-04-01 08:32:25 -0300

    feat: adiciona form para adicionar novos ingredientes na receita

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -65,6 +65,13 @@ export class RecipeEditComponent implements OnInit {
     console.log({...this.recipeForm.value})
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
+      'name': new FormControl(),
+      'amount': new FormControl()
+    }))
+  }
+
   ngOnDestroy() {
     this.routeSubscription.unsubscribe()
   }
