34c28dda0db99c8377bf05e474839bd3fcd9e3a2
guiziis <guigamoda@hotmail.com>
2024-04-28 10:08:05 -0300

    chore: adiciona environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  fireBaseUrl: 'https://ng-complete-guide-9da11-default-rtdb.firebaseio.com/posts.json',
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
